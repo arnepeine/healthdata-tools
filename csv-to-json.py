04064bdb7baaba01f969a6b6e149ad0cf8a88353
Arne Peine <arnepeine@gmail.com>
2018-02-20 12:22:33 +0100

    Update csv-to-json.py

--- csv-to-json.py
+++ csv-to-json.py
@@ -1,11 +1,11 @@
-# CSV to JSON
+# Convert CSV to JSON
 from pandas import read_csv
 i=1
 print("[")
-df = read_csv("file.csv" ,names = ["oscode", "name", "category"])
+df = read_csv("file.csv" ,names = ["number","oscode", "name", "category"])
 for index, row in df.iterrows():
     print("{")
-    print('"value":"'+row["oscode"]+'", "text": "'+row["oscode"],row["name"]+'", "class": "'+row["category"]+'", "oimid": "'+row["oscode"]+'"')
+    print('"value":"'+ str(row["number"]) +'", "text": "'+row["oscode"],row["name"]+'", "class": "'+row["category"]+'", "oimid": "'+row["oscode"]+'"') 
     if i == len(df):
         print("}") 
     else:
