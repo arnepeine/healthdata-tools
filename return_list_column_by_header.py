236793290196361d7badfbe171b78797ea6be266
Arne Peine <arnepeine@gmail.com>
2018-06-08 15:03:52 +0200

    Update return_list_column_by_header.py

--- return_list_column_by_header.py
+++ return_list_column_by_header.py
@@ -1,3 +1,4 @@
+# Split Dataset by header and data, check if column label is present, then put out a list of values.
 class Dataset:
     def __init__(self, data):
         self.header = data[0]
@@ -7,8 +8,7 @@ class Dataset:
             if label in value:
                 list_of_data = []
                 for row in self.data:
-                    list_of_data.append(value)
+                    list_of_data.append(row[idx])
                 return list_of_data
             else:
                 return None
-        
