ed729863b9daed641fdcc23db7c1d758507695c1
Arne Peine <arnepeine@gmail.com>
2019-05-08 11:55:16 +0200

    Create eicu-gender-dischargestatus.sql

---
+++ eicu-gender-dischargestatus.sql
@@ -0,0 +1,11 @@
+
+SELECT pt.patientunitstayid, pt.age, pt.apacheadmissiondx,
+       CASE WHEN pt.gender = 'Male' THEN 1
+            WHEN pt.gender = 'Female' THEN 2
+            ELSE NULL END AS gender,
+       CASE WHEN pt.hospitaldischargestatus = 'Alive' THEN 0
+            WHEN pt.hospitaldischargestatus = 'Expired' THEN 1
+            ELSE NULL END AS hosp_mortality,
+       ROUND(pt.unitdischargeoffset/60) AS icu_los_hours
+FROM eicu_crd.patient pt
+ORDER BY pt.patientunitstayid;
